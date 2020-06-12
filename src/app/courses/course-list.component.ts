ff06bc17aee30c9208e5ba305d6bacf1fc43f793
Lucas5h5h <lucas.inc.74@gmail.com>
2020-06-12 18:09:04 -0400

    5 files modified, 3 added, 1 deleted

---
+++ src/app/courses/course-list.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from "@angular/core";
+import {Course} from './course';
+
+@Component({
+  selector:'app-course-list',
+  templateUrl:'./course-list.component.html'
+})
+export class CourseListComponent implements OnInit { 
+  courses: Course[] = [];
+
+  ngOnInit(): void {
+    this.courses = [{
+      id: 1,
+      name: 'Angular 8',
+      imageUrl:'',
+      price: 99.99,
+      code: 'xps-5477',
+      duration: 120,
+      rating: 5.4,
+      realeaseDate: 'December, 4, 2020'
+    },{
+      id: 2,
+      name: 'JavaScript',
+      imageUrl:'',
+      price: 59.99,
+      code: 'xps-8798',
+      duration: 120,
+      rating: 6.4,
+      realeaseDate: 'April, 17, 2020'
+    }
+    ]
+  }
+}
